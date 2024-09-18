f03dc9dd5b2b847fd5047d3b0be03c6791346028
Hagda Gomes <222.14.070@EDU.gruposete.local>
2024-09-17 22:23:42 -0300

    primeiro commit

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#9c44dc",
+  
+      }}
+
+    >
+
+      <Text>Olá, Hagda</Text>
+      <Text> Saldo Disponível: R$0,01</Text>
+      <Text> Fatura atual: R$100,00 </Text>
+  
+
+    </View>
+
+  );
+
+
+
+
+}
